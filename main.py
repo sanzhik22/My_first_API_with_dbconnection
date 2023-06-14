dd0191c8544add74e51f638daa9477adf860de93
Sanzhar <anapiyaevs0519@gmail.com>
2023-06-14 14:07:40 +0600

    [main.py][L] - added method select airport which works, also added method add airport which gives 400 error

--- main.py
+++ main.py
@@ -5,32 +5,45 @@ from dotenv import load_dotenv
 import requests
 
 select_airport = (
-    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+    "select * from src_airports where city = %s"
 )
-CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL, 
-                        date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
+# CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL,
+#                         date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
+#
+# INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+insert_airport = (
+    "INSERT INTO src_airport (airport_code, airport_name, city, coordinates, timezone) VALUES (%s, %s, %s, %s, %s);")
 
-INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
-INSERT_TEMP = (
-    "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
-)
+
+
 
 con = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='postgres')
 
+def search_airport(location):
+    cursor = con.cursor()
+    cursor.execute(select_airport, (location,))
+    airports = cursor.fetchall()
+    return airports
+
+def add_airport(airport_code, airport_name, city, coordinates, timezone):
+    cursor = con.cursor()
+    cursor.execute(insert_airport, (airport_code,),(airport_name,), (city,), (coordinates,), (timezone,))
+
 app =Flask(__name__)
 
-@app.post('/api/room')
-def create_room():
+@app.post('/my_loction')
+def send_post():
     data = request.get_json()
-    name = data['name']
-    with con:
-        with  con.cursor() as cursor:
-            cursor.execute(CREATE_ROOMS_TABLE)
-            cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
-            room_id = cursor.fetchone()[0]
-    return {'id': room_id, 'message': f'Room {name} created'}, 201
+    location = data['city']
+    answer = search_airport(location)
+    for row in answer:
+        return f'В городе {location}, находится аэропорт {row[1]} котрый нахоитсься по координатам: {row[3]}', 201
 
-
+@app.post('/add_airport')
+def add_air():
+    data = request.get_json()
+    add_airport(data['airport_code'], data['airport_name'], data['city'], data['coordinates'], data['timezone'])
+    return 201
 
 if __name__ == '__main__':
     app.run(debug=True)
