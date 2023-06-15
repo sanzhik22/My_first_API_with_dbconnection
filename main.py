b00cb57f8301a8f3eadc5d8c3c5d9b7bd2b1a1d4
Sanzhar <anapiyaevs0519@gmail.com>
2023-06-15 10:47:31 +0600

    [main.py][S] - fixed some bags

--- main.py
+++ main.py
@@ -13,39 +13,6 @@ select_airport = "select * from src_airports where city = %s"
 
 select_airport_by_code = "select * from src_airports where airport_code = %s"
 
-# con = psycopg2.connect(host=host, database=database, user=user, password=password)
-
-
-# def connect():
-#     """ Connect to the PostgreSQL database server """
-#     conn = None
-#     try:
-#         # connect to the PostgreSQL server
-#         print('Connecting to the PostgreSQL database...')
-#         conn = psycopg2.connect(host=host, database=database, user=user, password=password)
-#
-#         # create a cursor
-#         cur = conn.cursor()
-#
-#         # execute a statement
-#         print('PostgreSQL database version:')
-#         cur.execute('SELECT version()')
-#
-#         # display the PostgreSQL database server version
-#         db_version = cur.fetchone()
-#         print(db_version)
-#
-#         # close the communication with the PostgreSQL
-#         cur.close()
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print('Database connection closed.')
-
-
-
 
 def search_airport(location):
     try:
